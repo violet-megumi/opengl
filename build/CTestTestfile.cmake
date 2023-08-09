8916628f0ca60fc93fa9e05d746d0d0c672c3d20
nieteng <nieteng@meituan.com>
2023-08-09 16:35:00 +0800

    lenrnopengl_capter2

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/violet/Desktop/my_project/opengl
+# Build directory: /Users/violet/Desktop/my_project/opengl/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
